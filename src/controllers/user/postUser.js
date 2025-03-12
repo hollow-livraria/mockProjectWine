fad33e17ea760ce61dac9f35082b6164c68cf6da
livraria <nicollasfsmoreira@gmail.com>
2025-03-12 15:08:37 -0300

    prisma adicionar e adeus compra

--- src/controllers/user/postUser.js
+++ src/controllers/user/postUser.js
@@ -1,17 +1,29 @@
-export default function postUser(req, res) {
+import { create } from "../../models/userModel.js"
+
+export default async function postUser(req, res) {
+  const user = req.body
+  const result = await create(user)
+
+  if(!result) {
+    return res.status(500).json({
+      message: "erro ao criar o usuario!"
+    })
+  }
+
+  delete result.password
+
   return res.json({
     message: "conta criada com sucesso.",
-    user: {
-      id: 1,
-      name: "livrariazin",
-      email: "livros@gmail.com",
-      senha: "placeholder",
-      nascimento: "07/07/2004",
-      endereço: "Rua principal",
-      cidade: "Goias",
-      estado: "MG",
-      cpf: "999.999.999-99",
-      telefone: "99 9999-9999",
-    },
+    user: result
+    //   id: 1,
+    //   name: "livrariazin",
+    //   email: "livros@gmail.com",
+    //   senha: "placeholder",
+    //   nascimento: "07/07/2004",
+    //   endereço: "Rua principal",
+    //   cidade: "Goias",
+    //   estado: "MG",
+    //   cpf: "999.999.999-99",
+    //   telefone: "99 9999-9999",
   });
 }
--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ import express from "express";
 import userRouter from "./routers/users.js";
 import vinhoRouter from "./routers/vinho.js";
 import compraRouter from "./routers/compra.js";
-import itemsCompraRouter from "./routers/itemsCompra.js";
 
 const app = express();
+
+app.use(express.json()) //le o json e grava o objeto no req.body
 
 app.use("/user", userRouter);
 app.use("/vinho", vinhoRouter);
 app.use("/compra", compraRouter);
-app.use("/items", itemsCompraRouter);
 
 app.listen(3000, () => {
   console.log("o server ai http://localhost:3000");
